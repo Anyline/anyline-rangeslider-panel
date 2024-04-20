7a8a532adeecec1167c41a54cd7316a53190bfc4
Aditya Singh <aditya.singh@anyline.com>
2024-04-20 09:38:10 +0200

    chore: use getTemplateSrv to retrieve selectedVariableValue

--- src/components/SimplePanel.tsx
+++ src/components/SimplePanel.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { PanelProps } from '@grafana/data';
+import { PanelProps, TypedVariableModel } from '@grafana/data';
 import MultiRangeSlider from 'multi-range-slider-react';
-import { locationService } from '@grafana/runtime';
+import { getTemplateSrv, locationService } from '@grafana/runtime';
 import { SimpleOptions } from '../types';
 import findMinAndMaxValues from './utils/findMinAndMaxValues';
 
-const DEFAULT_PREFIX = "["
-const DEFAUT_DELIMITER = "TO"
-const DEFAULT_SUFFIX = "]"
+const DEFAULT_PREFIX = '[';
+const DEFAUT_DELIMITER = 'TO';
+const DEFAULT_SUFFIX = ']';
 
 type RangeChangeEvent = {
   minValue: number;
@@ -17,26 +17,32 @@ type RangeChangeEvent = {
 interface Props extends PanelProps<SimpleOptions> {}
 
 const SimplePanel: React.FC<Props> = ({ options }) => {
+  const selectedVariable = getTemplateSrv()
+    .getVariables()
+    .filter((variable) => {
+      return variable.name === options.variableName;
+    });
 
-  const urlParams = new URLSearchParams(window.location.search);
+  let selectedVariableValue = null;
 
-  const selectedVariableValue = urlParams.get(`var-${options.variableName}`);
+  if (selectedVariable.length > 0 && 'current' in selectedVariable[0]) {
+    selectedVariableValue = selectedVariable[0].current.value as string;
+  }
 
   const { minValue, maxValue } = findMinAndMaxValues(selectedVariableValue);
 
   const [_minValue, setMinValue] = useState<string>(minValue);
   const [_maxValue, setMaxValue] = useState<string>(maxValue);
 
-
   const handleInput = (e: RangeChangeEvent) => {
     let { minValue, maxValue } = e;
     let variableValue;
 
-    if(minValue < options.variableMinimumThreshold || minValue > options.variableMaximumThreshold) {
+    if (minValue < options.variableMinimumThreshold || minValue > options.variableMaximumThreshold) {
       minValue = options.variableMinimumThreshold;
     }
 
-    if(maxValue > options.variableMaximumThreshold || maxValue < options.variableMinimumThreshold) {
+    if (maxValue > options.variableMaximumThreshold || maxValue < options.variableMinimumThreshold) {
       maxValue = options.variableMaximumThreshold;
     }
 
@@ -46,11 +52,17 @@ const SimplePanel: React.FC<Props> = ({ options }) => {
     if (minValue === maxValue) {
       variableValue = `${minValue}`;
     } else {
-      variableValue = `${options.rangePrefix || DEFAULT_PREFIX}${minValue}${options.delimiterSpace ? ' ' : ''}${options.rangeDelimiter || DEFAUT_DELIMITER}${options.delimiterSpace ? ' ' : ''}${maxValue}${options.rangeSuffix || DEFAULT_SUFFIX}`;
+      variableValue = `${options.rangePrefix || DEFAULT_PREFIX}${minValue}${options.delimiterSpace ? ' ' : ''}${
+        options.rangeDelimiter || DEFAUT_DELIMITER
+      }${options.delimiterSpace ? ' ' : ''}${maxValue}${options.rangeSuffix || DEFAULT_SUFFIX}`;
     }
 
     locationService.partial({ [`var-${options.variableName}`]: variableValue }, true);
   };
+
+  if (!selectedVariable) {
+    return <div style={{ padding: '8px' }}>No variable selected yet</div>;
+  }
 
   return (
     <div style={{ padding: '8px' }}>
